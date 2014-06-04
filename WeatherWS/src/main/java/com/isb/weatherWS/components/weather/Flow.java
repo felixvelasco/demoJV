2d70b9537efd761dd4a1aa8ae09133c0c4557fe1
Félix Velasco <fvelascov@ext.vectorsf.com>
2014-06-04 10:39:54 +0200

    Web service

--- WeatherWS/src/main/java/com/isb/weatherWS/components/weather/Flow.java
+++ WeatherWS/src/main/java/com/isb/weatherWS/components/weather/Flow.java
@@ -2,8 +2,13 @@ package com.isb.weatherWS.components.weather;
 
 import java.io.Serializable;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import com.cdyne.ws.weatherws.WeatherReturn;
+import com.cdyne.ws.weatherws.WeatherSoap;
+import com.vectorsf.jvoiceframework.core.service.ws.WebServiceProvider;
 
 @Component("com_isb_weatherWS_components_weather_Flow")
 @Scope("prototype")
@@ -11,8 +16,16 @@ public class Flow implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Autowired
+	private WebServiceProvider wsprovider;	
+
 	public WeatherResult getWeatherInfo(String zipCode) {
-		// TODO
-		return null;
+	
+		WeatherSoap weatherClient = wsprovider.getClient(WeatherSoap.class, "WeatherSoap");
+		
+		WeatherReturn response = weatherClient.getCityWeatherByZIP(zipCode);
+		
+		WeatherResult ret = new WeatherResult(response);
+		return ret;
 	}
 }
